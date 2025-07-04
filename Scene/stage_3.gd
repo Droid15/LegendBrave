f60483ae15bfcf647796f1369f3036c842bf41c2
liyiru <liyiru@youloft.com>
2025-07-04 16:26:28 +0800

    优化ui

--- Scene/stage_3.gd
+++ Scene/stage_3.gd
@@ -16,9 +16,14 @@ func _on_timer_timeout() -> void:
 		var pos := Vector2(3226,345)
 		SceneMaster.spawn_bee(pos)
 
+#看门野猪进入摄像机视野范围
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
 	show_enemy_boar = true
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	show_enemy_boar = false
+
+#看门野猪被回收
+func _on_visible_on_screen_notifier_2d_tree_exited() -> void:
 	show_enemy_boar = false
